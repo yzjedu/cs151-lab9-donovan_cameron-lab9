1bac433b492ff8d14240058275bae2b03572b145
cameron <cjcombariza@loyola.edu>
2024-11-06 16:07:46 -0500

    basic algorithm

---
+++ main.py
@@ -0,0 +1,15 @@
+# Programmers:  Donovan, Cameron
+# Course:  CS151,
+# Due Date: november 14th
+# Lab Assignment:  lab09
+# Problem Statement:  You are organizing a dinner party with assigned seats. Create a program to read in all the attendees and then output the seat assignments.
+# Data In: file name
+# Data Out:  table #, seat # and name from list
+# Credits:
+# Input Files: [description of the format of the input files you need for this program to work]
+
+
+
+# Purpose:  [what problem does the function solve?]
+  # Parameters: [list with purpose in the same order they appear in the function header]
+  # Return: [return value, it's type, and what it represents]
